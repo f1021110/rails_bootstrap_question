04ce1806526a042bb36636375d09bba041630862
kanekoyosuke <f85601955@gmail.com>
2020-07-31 23:29:14 +0900

    sala

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    validates:name, presence: true
+    validates:name, presence: true, length:{maximum:15}
     validates:email, presence: true
 
+    VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$/i
     has_secure_password
+    validates:password, presence: true,format: { with: VALID_PASSWORD_REGEX }
+
 end
