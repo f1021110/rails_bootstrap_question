62c3bfe87ff9a6eaf23126be41c1589a79c7198e
kanekoyosuke <f85601955@gmail.com>
2020-08-01 14:07:17 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates:name, presence: true, length:{maximum:15}
     validates:email, presence: true
 
-    VALID_PASSWORD_REGEX = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$/i
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
     has_secure_password
     validates:password, presence: true,format: { with: VALID_PASSWORD_REGEX }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'session/new'
   get 'users/new'
   root 'pages#index'
   get 'pages/help'
 
   resources :users
+
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 end
